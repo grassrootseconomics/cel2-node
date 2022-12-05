a1048444dd67f185d6a1ae06da23fbc7753faa1b
Mohamed Sohail <sohailsameja@gmail.com>
2022-12-05 13:25:31 +0300

    fix: permission cmds

--- docker/alfajores/sync_snapshot.sh
+++ docker/alfajores/sync_snapshot.sh
@@ -2,5 +2,7 @@
 # docker-compose down
 
 wget https://storage.googleapis.com/celo-chain-backup/alfajores/chaindata-latest.tar.gz && \
-tar -xzf chaindata-latest.tar.gz && \
-mv chaindata data/celo
+tar -xzf chaindata-latest.tar.gz
+
+# sudo rm -rf data/celo/chaindata
+# sudo mv chaindata data/celo
